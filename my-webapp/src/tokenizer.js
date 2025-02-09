24986062675ba2c682eec26a7f5d49b03dc6a9f0
ujjalcal <ujjal.b@gmail.com>
2025-02-09 15:57:11 +0000

    updates - broken

--- my-webapp/src/tokenizer.js
+++ my-webapp/src/tokenizer.js
@@ -1,9 +1,9 @@
 // filepath: tokenizer.js
 const tiktoken = require('tiktoken');
 
-function tokenize_text(text, encoding_name = 'cl100k_base') {
+function tokenizeText(text, encoding_name = 'cl100k_base') {
     const encoding = tiktoken.get_encoding(encoding_name);
     return encoding.encode(text);
 }
 
-module.exports = { tokenize_text };
+module.exports = { tokenizeText };
